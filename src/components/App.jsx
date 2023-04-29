0dec474096f1e21404390799790bbbacf45d81f7
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-04-29 13:16:39 +0300

    final update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
+import React, { Component } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Notification from './Notification/Notification';
+import Section from './Section';
+import s from './App.module.css';
+
+class App extends Component {
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+
+  
+leaveFeedback = (feedback) => {
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1,
+    }));
+  };
+  
+  countTotalFeedback = () => {
+    const totalFeedback = Object.values(this.state);
+    return totalFeedback.reduce((acc, value) => {
+      acc += value;
+      return acc;
+    })     
+  } 
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.trunc(this.state.good / this.countTotalFeedback() * 100);
+  }
+
+  
+  render() {
+    const btns = Object.keys(this.state);
+    const stats = Object.entries(this.state);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.container}>
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={btns}
+            onLeaveFeedback={this.leaveFeedback}
+        />
+        </Section>
+           <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? <Statistics
+            state={stats}
+          total={this.countTotalFeedback()}
+          posPersentage={this.countPositiveFeedbackPercentage()}/>
+                : <Notification message="There is no feedback"/>}
+               </Section> 
+      </div>
     </div>
   );
+  }
+
 };
+
+export default App;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,16 @@
+import s from './Section.module.css';
+import PropTypes from 'prop-types';
+
+const Section = ({ title, children }) => (
+    <section className={s.section}>
+        <h2 className={s.header}>{title}</h2>
+        {children}
+    </section>
+);
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+}
+
+export default Section;
